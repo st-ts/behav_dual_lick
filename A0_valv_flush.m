55892e9d75577d9b8099d118b78bb3bf721ca20e
Anya Stetsenko <stetsenko.an@gmail.com>
2023-07-13 10:03:18 -0400

    Update A0_valv_flush.m

--- A0_valv_flush.m
+++ A0_valv_flush.m
@@ -1,8 +1,12 @@
+%% % Flush valves
+% Since after some time withut use some air accumulates in the valves,
+% Before running the training/experiments, valves need to be flushed 
 
+clear variables; close all;
+% Set the time of flushing the valves; typical value for the 1 day of not 
+% used is 25 seconds
+dur_flush = 25;
 
-% Flush valves
-clear all; close all;
-% Parameters to test
 pause_dur = 0.4;
 
 % The pics for valves
@@ -11,11 +15,10 @@ pin_valve_r = 15; %
 
 % Initialize rasp
 mypi = raspi();
-%configurePin(mypi,pin_valve_r,'DigitalOutput');
 
 % Let's go!
 tic
-for i = 1:25
+for i = 1:dur_flush
     writeDigitalPin(mypi,pin_valve_r,1);
     pause(pause_dur);
     writeDigitalPin(mypi,pin_valve_r,0);
