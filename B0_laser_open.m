53a17f957c4a9c1b13bc277e6d13c7870e40c5b6
st-ts <stetsenko.an@gmail.com>
2023-12-05 18:20:29 -0500

    -

---
+++ B0_laser_open.m
@@ -0,0 +1,11 @@
+%% Laser test
+% 
+% close all; clear variables; format compact;
+% 
+% % Initialize Raspberry Pi
+% rasp_init;
+
+    % Left
+    writePosition(servo_las_L,servo_las_open_L);
+    % Right
+    writePosition(servo_las_R,servo_las_open_R);
